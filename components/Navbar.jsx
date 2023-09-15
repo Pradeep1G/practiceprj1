ede01fa7a60b4f44f5253d23badb4654619b880b
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-09-16 00:51:32 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import "../src/App.css"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" className="nav--icon" />
+        <img src={logo} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
